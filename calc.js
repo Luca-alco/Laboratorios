70b72a2b475545c92b0f8f6d35d81f9c88e3e661
luca-alco <luca.tomas03@gmail.com>
2025-03-28 15:25:25 -0300

    corregido

--- calc.js
+++ calc.js
@@ -1,39 +1,58 @@
-function sumar () {
-    n1 =  document.getElementById('num1').value;
-    num2 =  document.getElementById('num2').value;
+console.log("calc.js cargado correctamente");
 
-    resultado = parseInt(num1) + parseInt(num2);
+function sumar () {
+   let num1 =  document.getElementById('num1').value;
+   let num2 =  document.getElementById('num2').value;
+
+   let numero1 = Number(num1)
+   let numero2 = Number(num2)
+
+    resultado = numero1 + numero2;
     console.log(resultado);
 
     document.getElementById('resultado').innerHTML = resultado;
 }
 
 function restar () {
-    num1 =  document.getElementById('num1').value;
-    num2 =  document.getElementById('num2').value;
+    let num1 =  document.getElementById('num1').value;
+    let num2 =  document.getElementById('num2').value;
 
-    resultado = parseInt(num1) - parseInt(num2);
+   let numero1 = Number(num1)
+   let numero2 = Number(num2)
+
+    resultado = numero1 - numero2;
     console.log(resultado);
 
     document.getElementById('resultado').innerHTML = resultado;
 }
 
 function multiplicar () {
-    num1 =  document.getElementById('num1').value;
-    num2 =  document.getElementById('num2').value;
+    let num1 =  document.getElementById('num1').value;
+    let num2 =  document.getElementById('num2').value;
 
-    resultado = parseInt(num1) * parseInt(num2);
+   let numero1 = Number(num1)
+   let numero2 = Number(num2)
+
+    resultado = numero1 * numero2;
     console.log(resultado);
 
     document.getElementById('resultado').innerHTML = resultado;
 }
 
 function dividir () {
-    num1 =  document.getElementById('num1').value;
-    num2 =  document.getElementById('num2').value;
+    let num1 =  document.getElementById('num1').value;
+    let num2 =  document.getElementById('num2').value;
 
-    resultado = parseInt(num1) / parseInt(num2);
+   let numero1 = Number(num1)
+   let numero2 = Number(num2)
+
+   if (numero2 == 0) {
+        document.getElementById('resultado').innerHTML ="Math error";o;
+        return;
+    }
+
+    resultado = numero1 / numero2;
     console.log(resultado);
 
-    document.getElementById('resultado').innerHTML = resultado;
+    document.getElementById('resultado').innerHTML = resultado;o;
 }
