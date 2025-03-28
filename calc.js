807e6ce04b87a308b6ee11ac1fc11a06e9ceb280
luca-alco <luca.tomas03@gmail.com>
2025-03-28 14:56:03 -0300

    cambio de variable

--- calc.js
+++ calc.js
@@ -1,5 +1,5 @@
 function sumar () {
-    num1 =  document.getElementById('num1').value;
+    n1 =  document.getElementById('num1').value;
     num2 =  document.getElementById('num2').value;
 
     resultado = parseInt(num1) + parseInt(num2);
