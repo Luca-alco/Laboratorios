9ec41b81a2e4e5d38137e0d2413e1f7fa3ceb4e2
luca-alco <luca.tomas03@gmail.com>
2025-03-27 22:25:27 -0300

    corregirlos

---
+++ calc.js
@@ -0,0 +1,39 @@
+function sumar () {
+    num1 =  document.getElementById('num1').value;
+    num2 =  document.getElementById('num2').value;
+
+    resultado = parseInt(num1) + parseInt(num2);
+    console.log(resultado);
+
+    document.getElementById('resultado').innerHTML = resultado;
+}
+
+function restar () {
+    num1 =  document.getElementById('num1').value;
+    num2 =  document.getElementById('num2').value;
+
+    resultado = parseInt(num1) - parseInt(num2);
+    console.log(resultado);
+
+    document.getElementById('resultado').innerHTML = resultado;
+}
+
+function multiplicar () {
+    num1 =  document.getElementById('num1').value;
+    num2 =  document.getElementById('num2').value;
+
+    resultado = parseInt(num1) * parseInt(num2);
+    console.log(resultado);
+
+    document.getElementById('resultado').innerHTML = resultado;
+}
+
+function dividir () {
+    num1 =  document.getElementById('num1').value;
+    num2 =  document.getElementById('num2').value;
+
+    resultado = parseInt(num1) / parseInt(num2);
+    console.log(resultado);
+
+    document.getElementById('resultado').innerHTML = resultado;
+}
