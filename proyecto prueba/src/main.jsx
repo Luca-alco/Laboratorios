019336c95e4bfd263459bdf27de2531a502275d4
Alan Omonte <alan.omt49@gmail.com>
2025-04-24 17:26:18 -0300

    Correcciones al main

--- proyecto prueba/src/main.jsx
+++ proyecto prueba/src/main.jsx
@@ -1,31 +1,24 @@
 // src/main.jsx
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import MyProfile from './pages/myProfile'; 
-import Products from './pages/products'; 
-import Prueba from './pages/prueba'; // Asegúrate de que la ruta sea correcta
-import HomeScreen from './pages/homescreen'; 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MyProfile from "./pages/myProfile";
+import Products from "./pages/products";
+import HomeScreen from "./pages/homescreen";
 // import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import PubliNueva from './pages/PubliNueva';
-import './pages/index.css';
-import Carrito from './pages/Carrito';
+import PubliNueva from "./pages/PubliNueva";
+import "./pages/index.css";
+import Carrito from "./pages/Carrito";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-
   <React.StrictMode>
-
-
-   
-   <HomeScreen/>
-   {/*<MyProfile/>*/}
-   {/*<Products/>*/}
-
-
-
+    <Carrito />
+    {/* <PubliNueva /> */}
+    {/* <HomeScreen /> */}
+    {/* <MyProfile /> */}
+    {/* <Products /> */}
   </React.StrictMode>
-
 );
