3f66159b61a6624b5ea4f58ec5eae3cf27e805da
luca-alco <luca.tomas03@gmail.com>
2025-05-08 01:02:14 -0300

    .

--- Proyecto/src/App.jsx
+++ Proyecto/src/App.jsx
@@ -2,28 +2,32 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomeScreen from "./pages/homescreen";
 import MyProfile from "./pages/myProfile";
-import Products from "./pages/products"; //falta una ruta para el producto individual
+import Products from "./pages/products";
 import Gstprod2 from "./pages/gstprod2";
 import PubliNueva from "./pages/PubliNueva";
 import Carrito from "./pages/Carrito";
 import UsersLogin from "./pages/usersLogin";
 import Register from "./pages/register";
 import ListaCatalogo from "./pages/ListaCatalogo";
+import ResponsiveFooter from "./pages/responsivefooter"; // Importa el footer
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomeScreen />} />
-        <Route path="/perfil" element={<MyProfile />} />
-        <Route path="/productos" element={<ListaCatalogo />} />
-        <Route path="/products/:id" element={<Products />} />
-        <Route path="/login" element={<UsersLogin />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/carrito" element={<Carrito />} />
-        <Route path="/gestion-productos" element={<Gstprod2 />} />
-        <Route path="/nueva-publicacion" element={<PubliNueva />} />
-      </Routes>
+      <div className="appContainer">
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="/perfil" element={<MyProfile />} />
+          <Route path="/productos" element={<ListaCatalogo />} />
+          <Route path="/products/:id" element={<Products />} />
+          <Route path="/login" element={<UsersLogin />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/carrito" element={<Carrito />} />
+          <Route path="/gestion-productos" element={<Gstprod2 />} />
+          <Route path="/nueva-publicacion" element={<PubliNueva />} />
+        </Routes>
+        <ResponsiveFooter /> 
+      </div>
     </BrowserRouter>
   );
 }
